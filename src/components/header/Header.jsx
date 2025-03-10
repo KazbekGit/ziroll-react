aead80ae7a12a4d82c00c747eb4cc82de82d5055
Kazbekgit <kazbekgit@gmail.com>
2025-03-10 17:59:38 +0300

    add padd shadows

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,11 +4,19 @@ import styles from "./Header.module.css";
 const Header = () => {
   return (
     <header className={styles.wrapper}>
-      <img src="vite.svg" width={40} alt="Vite Logo" />
+      <a href="/">
+        <img src="vite.svg" width={40} alt="Vite Logo" />
+      </a>
       <ul className={styles.nav}>
-        <li><a href="/">main</a></li>
-        <li><a href="#">news</a></li>
-        <li><a href="#">about</a></li>
+        <li>
+          <a href="/">main</a>
+        </li>
+        <li>
+          <a href="#">news</a>
+        </li>
+        <li>
+          <a href="#">about</a>
+        </li>
       </ul>
     </header>
   );
