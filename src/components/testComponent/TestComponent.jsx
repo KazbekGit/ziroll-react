5b4ccf78b18fa23bd57696a0dad3497fbae5ceb1
Kazbekgit <kazbekgit@gmail.com>
2025-03-17 17:21:50 +0300

    state test 2

--- src/components/testComponent/TestComponent.jsx
+++ src/components/testComponent/TestComponent.jsx
@@ -2,28 +2,16 @@ import React, { useState } from "react";
 import style from "./TestComponent.module.css";
 
 const testComponent = () => {
-  const [count, setCount] = useState(0);
+  const [sign, setSign] = useState(true);
 
-  const minus = () => {
-    setCount((prev) => prev - 1);
-    setCount((prev) => prev - 1);
-    setCount((prev) => prev - 1);
-  };
-
-  const plus = () => {
-    setCount(count + 1);
-    setCount(count + 1);
-    setCount(count + 1);
+  const handleSign = () => {
+    setSign((prev) => !prev);
   };
 
   return (
     <div className={style.wrapper}>
-      <div>{count}</div>
-      <p className={style.p} onClick={plus}>
-        +
-      </p>
-      <p className={style.p} onClick={minus}>
-        -
+      <p className={style.p} onClick={handleSign}>
+        {sign ? "+" : "-"}
       </p>
     </div>
   );
