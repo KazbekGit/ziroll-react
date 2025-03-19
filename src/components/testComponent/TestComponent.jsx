7b747b6c4e8d4c7597857a92e0afa1a534bdf0e2
Kazbekgit <kazbekgit@gmail.com>
2025-03-19 16:57:09 +0300

    forms

--- src/components/testComponent/TestComponent.jsx
+++ src/components/testComponent/TestComponent.jsx
@@ -1,20 +1,41 @@
 import React, { useState } from "react";
 import style from "./TestComponent.module.css";
 
-const testComponent = () => {
-  const [sign, setSign] = useState(true);
+const TestComponent = () => {
 
-  const handleSign = () => {
-    setSign((prev) => !prev);
+  const [images, setImages] = useState([])
+
+  const fetchData = (e) => {
+    e.preventDefault(); // Предотвращаем стандартную отправку формы
+    fetch("https://api.nekosia.cat/api/v1/images/catgirl", {
+      method: "GET", // Меняем на GET, так как это запрос данных
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`Ошибка: ${res.status}`);
+        }
+        return res.json();
+      })
+      .then((data) => setImages(data))
+      .catch((err) => console.error(err));
   };
 
   return (
     <div className={style.wrapper}>
-      <p className={style.p} onClick={handleSign}>
-        {sign ? "+" : "-"}
-      </p>
+      <form method="post" onSubmit={fetchData}>
+        <button
+          style={{
+            width: "100px",
+            height: "50px",
+            border: "1px solid grey",
+            borderRadius: "4px",
+          }}
+        >
+          Get cat's
+        </button>
+      </form>
     </div>
   );
 };
 
-export default testComponent;
+export default TestComponent;
